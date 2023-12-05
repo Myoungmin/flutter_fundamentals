a9c94de457918c75e837c854116be9caaebc85bd
AzureAD\고명민 <miung222@gmail.com>
2023-12-05 21:14:13 +0900

    간단한 Stateless Widget
    Stateless Widget은 StatelessWidget을 상속해야 한다.
    Stateless Widget은 Widget build(BuildContext context) 메서드를 오버라이딩 해야 한다.
    build 메서드를 통해 Widget이 표현하고자 하는 화면이 보여지게 된다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  // 참고: Key는 보통 위젯을 식별하기 위해 사용되며, 다음과 같이 키를 넣어줄 수 있다.
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    // Placeholder() 도 Plceholder Widget이다.
+    // 간단한 위젯으로 개발 시 단순히 위젯 위치를 알기 위해 사용한다.
+    return const Placeholder();
   }
 }

2aa7f1882fc0d3904f523c4e983c68c437e24c73
AzureAD\고명민 <miung222@gmail.com>
2023-12-12 22:13:38 +0900

    MaterialApp 내에 Scaffold 기본 위젯을 사용해서, 전체 화면을 표시

--- lib/main.dart
+++ lib/main.dart
@@ -5,135 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // 참고: Key는 보통 위젯을 식별하기 위해 사용되며, 다음과 같이 키를 넣어줄 수 있다.
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      textDirection: TextDirection.ltr,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 50,
-              height: 50,
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 50,
-              height: 50,
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 50,
-              height: 50,
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 50,
-              height: 50,
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 50,
-              height: 50,
-            ),
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.blue,
-              width: 50,
-              height: 50,
-            ),
-            Container(
-              color: Colors.yellow,
-              width: 50,
-              height: 50,
-            ),
-          ],
-        ),
-      ],
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Myounngmin')),
+      ),
     );
   }
 }

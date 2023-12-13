16051d69e22f9c27e62f66e7cb46439b2af4734a
AzureAD\고명민 <miung222@gmail.com>
2023-12-13 22:10:54 +0900

    Scaffold 위젯 사용법과 주요 property

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,58 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.red),
       home: Scaffold(
         appBar: AppBar(title: const Text('Myounngmin')),
+        body: const Center(
+          child: TextField(
+            decoration: InputDecoration(labelText: '입력해보세요'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          // The size of the shadow (default: 6)
+          elevation: 10.0,
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        drawer: Drawer(
+          child: ListView(
+            children: const <Widget>[
+              ListTile(
+                title: Text('Item 1'),
+              ),
+              ListTile(
+                title: Text('Item 2'),
+              ),
+            ],
+          ),
+        ),
+        persistentFooterButtons: const [
+          Icon(Icons.settings),
+          SizedBox(width: 5),
+          Icon(Icons.exit_to_app),
+          SizedBox(
+            width: 10,
+          ),
+        ],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 0,
+            fixedColor: Colors.green,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(Icons.home),
+              ),
+              BottomNavigationBarItem(
+                label: 'Search',
+                icon: Icon(Icons.search),
+              ),
+              BottomNavigationBarItem(
+                label: 'Profile',
+                icon: Icon(Icons.account_circle),
+              ),
+            ],
+            onTap: (int indexOfItem) {}),
+        backgroundColor: Colors.amberAccent,
+        resizeToAvoidBottomInset: false,
       ),
     );
   }

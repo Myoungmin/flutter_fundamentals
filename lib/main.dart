45c96f689c1d12fb497e279aeef337dd3b69e6f7
AzureAD\고명민 <miung222@gmail.com>
2023-12-20 22:39:03 +0900

    TextOverflow 적용 실습

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,53 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('TextStyle Property'),
+          title: const Text('Text Property'),
           backgroundColor: Colors.blue,
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Ko Myoungmin',
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                backgroundColor: Colors.blue,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 5,
-                wordSpacing: 20,
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.wavy,
-                decorationColor: Colors.red,
+          children: [
+            Container(
+              width: 400,
+              color: Colors.amber,
+              child: const Text(
+                'Welcome to Flutter My name is Ko Myoungmin',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 24),
+                softWrap: false,
+                overflow: TextOverflow.fade,
+              ),
+            ),
+            Container(
+              width: 400,
+              color: Colors.red,
+              child: const Text(
+                'Welcome to Flutter My name is Ko Myoungmin',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 24),
+                softWrap: false,
+                overflow: TextOverflow.clip,
+              ),
+            ),
+            Container(
+              width: 400,
+              color: Colors.green,
+              child: const Text(
+                'Welcome to Flutter My name is Ko Myoungmin',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontSize: 24),
+                softWrap: false,
+                overflow: TextOverflow.visible,
+              ),
+            ),
+            Container(
+              width: 400,
+              color: Colors.brown,
+              child: const Text(
+                'Welcome to Flutter My name is Ko Myoungmin',
+                textDirection: TextDirection.ltr,
+                textAlign: TextAlign.right,
+                style: TextStyle(fontSize: 24),
+                overflow: TextOverflow.visible,
               ),
             ),
           ],

e14185e2ac00a1401fe833c5312b4490d1812a65
AzureAD\고명민 <miung222@gmail.com>
2023-12-22 14:39:56 +0900

    Icon 위젯
    아이콘 표시 위젯
    Icons 클래스로 아이콘 이름을 지정

--- lib/main.dart
+++ lib/main.dart
@@ -15,103 +15,15 @@ class MyApp extends StatelessWidget {
           textDirection: TextDirection.ltr,
           children: [
             Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Image.network(
-                'https://avatars.githubusercontent.com/u/59222053?v=4',
-                width: 200,
-                height: 300,
-                fit: BoxFit.contain,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Image.network(
-                'https://avatars.githubusercontent.com/u/59222053?v=4',
-                width: 200,
-                height: 300,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Image.network(
-                'https://avatars.githubusercontent.com/u/59222053?v=4',
-                width: 200,
-                height: 300,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Image.network(
-                'https://avatars.githubusercontent.com/u/59222053?v=4',
-                width: 200,
-                height: 300,
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Image.network(
-                'https://avatars.githubusercontent.com/u/59222053?v=4',
-                width: 200,
-                height: 300,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(
-                  color: Colors.red,
-                  width: 2,
-                ),
-              ),
-              child: Image.network(
-                'https://avatars.githubusercontent.com/u/59222053?v=4',
-                width: 200,
-                height: 300,
-                fit: BoxFit.none,
+              child: const Icon(
+                textDirection: TextDirection.ltr,
+                Icons.home,
+                color: Colors.red,
+                size: 60.0,
               ),
             ),
           ],
-        )
+        ),
       ],
     );
   }

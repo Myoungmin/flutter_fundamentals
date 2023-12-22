552807ab9f8e05c99f86645830e7f1d361303605
AzureAD\고명민 <miung222@gmail.com>
2023-12-23 08:37:25 +0900

    IconButton 실습

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          textDirection: TextDirection.ltr,
-          children: [
-            Container(
-              child: const Icon(
-                textDirection: TextDirection.ltr,
-                Icons.home,
-                color: Colors.red,
-                size: 60.0,
-              ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Ko Myoungmin'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.comment),
+              tooltip: 'Comment Icon',
+              splashColor: Colors.red,
+              alignment: Alignment.topLeft,
+              highlightColor: Colors.brown,
+              hoverColor: Colors.amber,
+            ),
+            const IconButton(
+              // onPressed에 null을 할당하면 해당 위젯은 사용자의 입력을 받지 않으며, 일반적으로 시각적으로도 비활성화된 상태로 표시
+              onPressed: null,
+              icon: Icon(Icons.place),
+              tooltip: 'Comment Icon',
+              disabledColor: Colors.grey,
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }

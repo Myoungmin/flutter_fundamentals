f4615ac8f5bc55285d7c66cd501a7bfbfdf9f017
AzureAD\고명민 <miung222@gmail.com>
2023-12-26 21:47:20 +0900

    StatefulWidget 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  // TextEditingController로 TextField의 controller에 넣을 객체를 선언
-  final TextEditingController _emailController = TextEditingController();
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-              ),
-              TextField(
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  labelStyle: TextStyle(
-                    color: Colors.amber,
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.brown,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(width: 1, color: Colors.red),
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 5,
-                      color: Colors.blue,
-                    ),
-                  ),
-                  icon: Icon(Icons.settings),
-                  prefixIcon: Icon(Icons.phone),
-                  suffixIcon: Icon(Icons.star),
-                ),
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.search,
-                obscureText: true,
-                controller: _emailController,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print(_emailController.text);
-                },
-                child: const Text('Click'),
-              ),
-            ],
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _count++;
+              });
+            },
+            child: Text(
+              '$_count',
+            ),
           ),
         ),
       ),

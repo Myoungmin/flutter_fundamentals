cf33e9640169fbe9a6ad375c88614c76f010fe3c
AzureAD\고명민 <miung222@gmail.com>
2023-12-27 22:44:17 +0900

    Checkbox, Radio, Slider, Switch 실습

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,90 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _count = 0;
+  bool _checkBoxValue = false;
+  String _radioValue = 'Option 1';
+  double _sliderValue = 50.0;
+  bool _switchValue = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _count++;
-              });
-            },
-            child: Text(
-              '$_count',
-            ),
+        appBar: AppBar(
+          title: const Text('Form Controls Example'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                  child: Text(
+                      '$_checkBoxValue $_radioValue $_sliderValue $_switchValue')),
+              const SizedBox(
+                height: 16.0,
+              ),
+              const Text('Checkbox: '),
+              Checkbox(
+                value: _checkBoxValue,
+                onChanged: (value) {
+                  setState(() {
+                    // ! 없으면, A value of type 'bool?' can't be assigned to a variable of type 'bool'. 라는 에러메시지 표출
+                    _checkBoxValue = value!;
+                  });
+                },
+              ),
+              const SizedBox(height: 16.0),
+              const Text('Radio Buttons: '),
+              Row(
+                children: [
+                  Radio(
+                    value: 'Option 1',
+                    groupValue: _radioValue,
+                    onChanged: (value) {
+                      setState(() {
+                        _radioValue = value.toString();
+                      });
+                    },
+                  ),
+                  const Text('Option 1'),
+                  Radio(
+                    value: 'Option 2',
+                    groupValue: _radioValue,
+                    onChanged: (value) {
+                      setState(() {
+                        _radioValue = value.toString();
+                      });
+                    },
+                  ),
+                  const Text('Option 2'),
+                ],
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              const Text('Slider: '),
+              Slider(
+                value: _sliderValue,
+                onChanged: (value) {
+                  setState(() {
+                    _sliderValue = value;
+                  });
+                },
+                min: 0,
+                max: 100,
+              ),
+              const SizedBox(height: 16.0),
+              const Text('Switch: '),
+              Switch(
+                value: _switchValue,
+                onChanged: (value) {
+                  setState(() {
+                    _switchValue = value;
+                  });
+                },
+              )
+            ],
           ),
         ),
       ),

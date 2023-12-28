1be0f024c49b839f740f3897448827594e3ec6a0
AzureAD\고명민 <miung222@gmail.com>
2023-12-28 17:17:08 +0900

    Form 위젯 실습
    TextField는 단순히 하나의 텍스트 입력을 다루는데 반해서, Form은 그 자체로 입력 필드를 가지고 있지 않지만, FormField 위젯들을 그룹화하여 관리하여, 복잡한 유효성 검사를 보다 쉽게 할 수 있는 위젯이다.
    다른 위젯과 달리 Form 위젯은 자체적인 화면을 제공하지는 않으며, 사용자가 입력한 데이터의 유효성 검증, 데이터 관리 관련 기능을 제공한다.
    Form 위젯 내에서 TextFormField 위젯을 사용하여 각 데이터 입력을 받는 것이 일반적이다.

--- lib/main.dart
+++ lib/main.dart
@@ -12,90 +12,101 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool _checkBoxValue = false;
-  String _radioValue = 'Option 1';
-  double _sliderValue = 50.0;
-  bool _switchValue = false;
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  String _email = '';
+  String _password = '';
+  String _errorMessage = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Name: $_name');
+      print('Email: $_email');
+      print('Password: $_password');
+      setState(() {
+        _errorMessage = '';
+      });
+    } else {
+      setState(() {
+        _errorMessage = 'Please fill in all fields';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Form Controls Example'),
+          title: const Text('Form Example'),
         ),
         body: Container(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                  child: Text(
-                      '$_checkBoxValue $_radioValue $_sliderValue $_switchValue')),
-              const SizedBox(
-                height: 16.0,
-              ),
-              const Text('Checkbox: '),
-              Checkbox(
-                value: _checkBoxValue,
-                onChanged: (value) {
-                  setState(() {
-                    // ! 없으면, A value of type 'bool?' can't be assigned to a variable of type 'bool'. 라는 에러메시지 표출
-                    _checkBoxValue = value!;
-                  });
-                },
-              ),
-              const SizedBox(height: 16.0),
-              const Text('Radio Buttons: '),
-              Row(
-                children: [
-                  Radio(
-                    value: 'Option 1',
-                    groupValue: _radioValue,
-                    onChanged: (value) {
-                      setState(() {
-                        _radioValue = value.toString();
-                      });
-                    },
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Name',
                   ),
-                  const Text('Option 1'),
-                  Radio(
-                    value: 'Option 2',
-                    groupValue: _radioValue,
-                    onChanged: (value) {
-                      setState(() {
-                        _radioValue = value.toString();
-                      });
-                    },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your name';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
                   ),
-                  const Text('Option 2'),
-                ],
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              const Text('Slider: '),
-              Slider(
-                value: _sliderValue,
-                onChanged: (value) {
-                  setState(() {
-                    _sliderValue = value;
-                  });
-                },
-                min: 0,
-                max: 100,
-              ),
-              const SizedBox(height: 16.0),
-              const Text('Switch: '),
-              Switch(
-                value: _switchValue,
-                onChanged: (value) {
-                  setState(() {
-                    _switchValue = value;
-                  });
-                },
-              )
-            ],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _email = value!;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _password = value!;
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  child: const Text('Submit'),
+                ),
+                const SizedBox(height: 16.0),
+                Text(
+                  _errorMessage,
+                  style: const TextStyle(
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),

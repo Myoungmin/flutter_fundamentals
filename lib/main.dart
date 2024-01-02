1c966e4ffcea4bd0147dd0cf0efa8e2c89135074
AzureAD\고명민 <miung222@gmail.com>
2024-01-02 18:47:13 +0900

    ListView.builder를 이용하여 동적으로 리스트 아이템 생성

--- lib/main.dart
+++ lib/main.dart
@@ -4,108 +4,34 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.all(10),
-          children: [
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text('1'),
-              ),
-              title: const Text('Item1'),
-              subtitle: const Text('Item description'),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  print('click');
-                },
-              ),
-            ),
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text('2'),
-              ),
-              title: const Text('Item2'),
-              subtitle: const Text('Item description'),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  print('click');
-                },
-              ),
-            ),
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text('3'),
-              ),
-              title: const Text('Item3'),
-              subtitle: const Text('Item description'),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  print('click');
-                },
-              ),
-            ),
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text('4'),
-              ),
-              title: const Text('Item4'),
-              subtitle: const Text('Item description'),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  print('click');
-                },
-              ),
-            ),
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text('5'),
-              ),
-              title: const Text('Item5'),
-              subtitle: const Text('Item description'),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  print('click');
-                },
-              ),
-            ),
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.amber,
-                child: Text('6'),
-              ),
-              title: const Text('Item6'),
-              subtitle: const Text('Item description'),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () {
-                  print('click');
-                },
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Simple App'),
+      ),
+      body: ListView.builder(
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Item $index'),
+          );
+        },
       ),
     );
   }

a831ea657edefd27b7a4da44f6753e4aa87e014a
AzureAD\고명민 <miung222@gmail.com>
2024-01-07 20:11:01 +0900

    GridView에 IconButton으로 Container 추가 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  final List<String> items = List.generate(100, (index) => 'Item $index');
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int itemCount = 0;
+  List<String> items = [];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('GridView Example'),
+          title: const Text('GridView'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  itemCount++;
+                  items.add('Item $itemCount');
+                });
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.remove),
+              onPressed: () {
+                setState(() {
+                  items.removeLast();
+                  itemCount--;
+                });
+              },
+            )
+          ],
         ),
         body: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 8.0,
-          ),
+              crossAxisCount: 2, crossAxisSpacing: 8.0, mainAxisSpacing: 8.0),
           itemCount: items.length,
           itemBuilder: (context, index) {
             return Container(
-              color: Colors.blueGrey[100],
+              color: Colors.amber,
               child: Center(
                 child: Text(
                   items[index],
                   style: const TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
+                      fontSize: 25, fontWeight: FontWeight.bold),
                 ),
               ),
             );

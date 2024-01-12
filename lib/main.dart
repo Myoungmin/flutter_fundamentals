8e9cf88aa3dc225e4953a173026d32bb3fb59f2c
AzureAD\고명민 <miung222@gmail.com>
2024-01-13 00:18:46 +0900

    SizedBox와 Card 위젯 실습

--- lib/main.dart
+++ lib/main.dart
@@ -2,51 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TabBar Example',
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TabBar Example'),
-          bottom: TabBar(
-            controller: _tabController,
-            tabs: const <Widget>[
-              Tab(icon: Icon(Icons.home), text: 'Home'),
-              Tab(icon: Icon(Icons.star), text: 'Favorites'),
-              Tab(icon: Icon(Icons.settings), text: 'Settings'),
-            ],
+        body: Center(
+          child: SizedBox(
+            width: 200.0,
+            height: 100.0,
+            child: Card(
+              color: Colors.amber,
+              child: Text('Myoungmin'),
+            ),
           ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: const <Widget>[
-            Center(child: Text('Home')),
-            Center(child: Text('Favorites')),
-            Center(child: Text('Settings')),
-          ],
         ),
       ),
     );

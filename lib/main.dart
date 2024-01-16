23cc7ca25d7502b34bbb7a6293cf9f3a7125688e
AzureAD\고명민 <miung222@gmail.com>
2024-01-16 21:32:08 +0900

    BottomNavigation Bar 실습

--- lib/main.dart
+++ lib/main.dart
@@ -2,43 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: SizedBox(
-            width: 400,
-            height: 400,
-            child: Card(
-              elevation: 50,
-              margin: EdgeInsets.all(5),
-              shadowColor: Colors.blue,
-              color: Colors.amber,
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: Colors.deepPurpleAccent,
-                ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              activeIcon: Icon(
+                Icons.alarm,
+                color: Colors.red,
               ),
-              child: Center(
-                child: CircleAvatar(
-                  backgroundColor: Colors.brown,
-                  radius: 50,
-                  child: Center(
-                    child: Text(
-                      '1',
-                      style: TextStyle(fontSize: 80),
-                    ),
-                  ),
-                ),
-              ),
+              label: '홈',
             ),
-          ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: '즐겨찾기',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: '설정',
+            ),
+          ],
+          onTap: (value) {
+            setState(() {
+              _currentIndex = value;
+            });
+          },
+          currentIndex: _currentIndex,
+          backgroundColor: Colors.amber,
+          selectedItemColor: Colors.brown,
+          unselectedItemColor: Colors.blue,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
         ),
+        backgroundColor: Colors.grey,
       ),
     );
   }

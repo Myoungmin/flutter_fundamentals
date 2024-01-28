308f5c96038d2d9217f99f79fd317dc94a4018a4
AzureAD\고명민 <miung222@gmail.com>
2024-01-28 09:11:37 +0900

    Navigator.push로 새로운 페이지 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamentals/new_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-// ObjectKey에서 활용할 클래스 정의
-class Identity {
-  String name = '';
-  Identity(this.name);
-}
-
-class _MyAppState extends State<MyApp> {
-  List<Widget> tileList = [
-    // ValueKey 적용
-    MyStatefulWidget(Colors.brown, key: const ValueKey('Myoungmin')),
-    // ObjectKey 적용
-    MyStatefulWidget(Colors.green, key: ObjectKey(Identity('Myoungmin'))),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Row(
-              children: tileList,
-            ),
-            const SizedBox(
-              height: 50.0,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  tileList.insert(1, tileList.removeAt(0));
-                });
-              },
-              style:
-                  ElevatedButton.styleFrom(padding: const EdgeInsets.all(20)),
-              child: const Text(
-                "Click",
-                style: TextStyle(fontSize: 24),
-              ),
-            ),
-          ],
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  Color color;
-
-  MyStatefulWidget(this.color, {super.key});
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState(color);
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  Color color;
-
-  _MyStatefulWidgetState(this.color);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 200,
-      color: color,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go to new page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const NewPage(),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }

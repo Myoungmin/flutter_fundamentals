3b8270f10fae82431cce003c59383470f4acb352
AzureAD\고명민 <miung222@gmail.com>
2024-02-02 21:19:54 +0900

    DynamicWidget 실습

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,79 @@
 import 'package:flutter/material.dart';
-import 'new_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<DynamicWidget> listDynamic = [];
+
+  addDynamic() {
+    if (listDynamic.length >= 5) return;
+    listDynamic.add(DynamicWidget());
+    setState(() {});
+  }
+
+  submitData() {
+    for (var element in listDynamic) {
+      print(element.controller.text);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dynamic App'),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  itemCount: listDynamic.length,
+                  itemBuilder: (context, index) => listDynamic[index],
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  onPressed: submitData,
+                  child: const Text('Submit Data'),
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: addDynamic,
+          child: const Icon(
+            Icons.add,
+          ),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class DynamicWidget extends StatelessWidget {
+  DynamicWidget({super.key});
+
+  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Go to new Page'),
-          onPressed: () async {
-            final result = await Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) =>
-                    const NewPage(message: 'Hello from the home page!'),
-              ),
-            );
-            print('$result');
-          },
-        ),
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: TextField(
+        controller: controller,
+        decoration: const InputDecoration(hintText: 'Enter Data'),
       ),
     );
   }
